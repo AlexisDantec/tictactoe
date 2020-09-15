ebf4a03165f60ba8eab66432ad524dce125b1925
A <dantec.alexis@gmail.com>
2020-09-15 17:33:29 +0200

    some mistakes with global variable and the position

--- tic_tac_toe/morpion.py
+++ tic_tac_toe/morpion.py
@@ -1,9 +1,14 @@
 m =[[0,1,2,3],
-    [1,"x"," "," "],
+    [1," "," "," "],
     [2," "," "," "],
     [3," "," "," "]]
 row = 4
 column = 4
+c = 0
+b=0
+y=0
+x=0
+position= '0,0'
 def print_tab():
     for i in range(0, row):
         for j in range(0, column):
@@ -14,18 +19,32 @@ def print_tab():
             print("_", end = '_')
         print()
 print_tab()
-a = input('write position')
-b = len(a)
-c = int(a[0])
-d =int(a[2])
-if a[1] !="," or c<1 or c>3 or d<1 or d>3 or b!= 3:
-    print("wrong position")
-print(str(c)+ "et" +str(d))
-for i in range (0, row):
-    for j in range(0, column):
-        if i == c and j == d and m[i][j]==" ":
-            m[c][d] = "x"
-        elif m[i][j]=="x" or m[i][j]=="o":
-            print("the box is already used")
-print_tab()
+def where_place():
+    global position, b, y, x
+    position= input('write position : ')
+    b = len(position)
+    y = int(position[0])
+    x = int(position[2])
 
+
+def counter():
+    global c
+    c+=1
+if position[1] !="," or x<1 or x>3 or y<1 or y>3 or b!= 3:
+    print("wrong position")    
+def change_box():
+    for i in range (0, row):
+        for j in range(0, column):
+            if i == x and j == y and m[i][j]==" ":
+                if c%2 == 0:  
+                    m[x][y] = "x"
+                else:
+                    m[x][y]= "o"
+                counter()
+            elif m[i][j]=="x" or m[i][j]== "o" and x==i and y==j:
+                print('wrong position')
+
+while c<9:
+    where_place()
+    change_box()
+    print_tab()
