7f6a47445bd6c18eb198489e6df399919c21a7ae
A <dantec.alexis@gmail.com>
2020-09-15 15:39:41 +0200

    I translated some text in english

--- tic_tac_toe/morpion.py
+++ tic_tac_toe/morpion.py
@@ -2,30 +2,30 @@ m =[[0,1,2,3],
     [1,"x"," "," "],
     [2," "," "," "],
     [3," "," "," "]]
-nb_ligne = 4
-nb_colonne = 4
+row = 4
+column = 4
 def print_tab():
-    for i in range(0, nb_ligne):
-        for j in range(0, nb_colonne):
+    for i in range(0, row):
+        for j in range(0, column):
                 a = m[i][j]
                 print (str(a), end = '|')
         print()
-        for j in range(0, nb_colonne):
+        for j in range(0, column):
             print("_", end = '_')
         print()
 print_tab()
-a = input('ecrire les coordonnee')
+a = input('write position')
 b = len(a)
 c = int(a[0])
 d =int(a[2])
 if a[1] !="," or c<1 or c>3 or d<1 or d>3 or b!= 3:
-    print("mauvaise coordonnee")
+    print("wrong position")
 print(str(c)+ "et" +str(d))
-for i in range (0, nb_ligne):
-    for j in range(0, nb_colonne):
+for i in range (0, row):
+    for j in range(0, column):
         if i == c and j == d and m[i][j]==" ":
             m[c][d] = "x"
         elif m[i][j]=="x" or m[i][j]=="o":
-            print("case deja occupee")
+            print("the box is already used")
 print_tab()
 
