610e5e719ea57ca5e0669188d2beda5d48f80d48
A <dantec.alexis@gmail.com>
2020-09-16 16:37:51 +0200

    add win condition

--- tic_tac_toe/morpion.py
+++ tic_tac_toe/morpion.py
@@ -8,7 +8,11 @@ c = 0
 b=0
 y=0
 x=0
+win=0
 position= '0,0'
+ok=0
+win_x=1
+win_y=1
 def print_tab():
     for i in range(0, row):
         for j in range(0, column):
@@ -30,21 +34,68 @@ def where_place():
 def counter():
     global c
     c+=1
-if position[1] !="," or x<1 or x>3 or y<1 or y>3 or b!= 3:
-    print("wrong position")    
+def test_break():
+    global ok
+    ok =0
+    if position[1] !="," or x<1 or x>3 or y<1 or y>3 or b!= 3:
+        print("wrong position")
+    else:
+        ok=1
 def change_box():
-    for i in range (0, row):
-        for j in range(0, column):
-            if i == x and j == y and m[i][j]==" ":
-                if c%2 == 0:  
-                    m[x][y] = "x"
-                else:
-                    m[x][y]= "o"
-                counter()
-            elif m[i][j]=="x" or m[i][j]== "o" and x==i and y==j:
-                print('wrong position')
+    test_break()
+    if ok==1:
+        for i in range (0, row):
+            for j in range(0, column):
+                if i == x and j == y and m[i][j]==" ":
+                    if c%2 == 0:  
+                        m[x][y] = "x"
+                    else:
+                        m[x][y]= "o"
+                    counter()
+                elif m[i][j]=="x" and x==i and y==j or m[i][j]=="o" and x==i and y==j:
+                    print('wrong position')
+def win_row():
+    global win_x, win_y, win
+    win_x=1
+    win_y=1
+    while win_x<3:
+        while m[win_x][win_y] == m[win_x][win_y +1]:
+            if win_y ==2:
+                win=1
+                return
+            win_y+=1
+        win_y=1
+        win_x+=1
+def win_column():
+    global win_x, win_y, win
+    win_x=1
+    win_y=1
+    while win_y<3:
+        while m[win_x][win_y] == m[win_x+1][win_y]:
+            if win_x ==2:
+                win=1
+                return
+            win_x+=1
+        win_x=1
+        win_y+=1
+def win_diag():
+    global win
+    if m[1][1]== m[2][2] and m[2][2]== m[3][3] and m[2][2]!=" ":
+        win=1
+    elif m[3][1]== m[2][2] and m[2][2]== m[1][3] and m[2][2]!=" ":
+        win=1
 
-while c<9:
-    where_place()
-    change_box()
-    print_tab()
+
+
+while c<9 and win !=1:
+    if win !=1:
+        where_place()
+        change_box()
+        print_tab()
+        if c>3:
+            win_row()
+            win_column()
+            win_diag()
+        print(win)
+if win==0:
+    print('draw')
